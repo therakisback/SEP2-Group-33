481db81b06421a7bd14d15f0e510b3299f420441
rak <koenigryan4@gmail.com>
2024-02-13 09:27:57 +0000

    Added Java Project files

---
+++ Project File/src/Main.java
@@ -0,0 +1,16 @@
+
+
+/*
+ * "Controller" class for the game, manages the functionality
+ *    between the Game class running the game and the UI class
+ *    dealing with user IO
+ *
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
