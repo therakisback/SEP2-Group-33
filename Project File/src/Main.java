eb7804ea4d646294fbdf1132104fe08e0665fae6
rak <koenigryan4@gmail.com>
2024-02-13 10:26:32 +0000

    Update Main.java

--- Project File/src/Main.java
+++ Project File/src/Main.java
@@ -4,13 +4,13 @@
  * "Controller" class for the game, manages the functionality
  *    between the Game class running the game and the UI class
  *    dealing with user IO
- *
- *
+ * A framework for this class and function is difficult,
+ *    as it will rely on every other class
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // TODO
     }
 }
 
