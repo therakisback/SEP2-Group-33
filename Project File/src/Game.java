08cb8aadfe6e5a1afe6fc03bd34f434b9291c6cf
rak <koenigryan4@gmail.com>
2024-02-13 10:47:42 +0000

    Update Game.java outline

--- Project File/src/Game.java
+++ Project File/src/Game.java
@@ -1,4 +1,4 @@
-//import
+import java.util.Random;
 
 /**
  * "Game Manger" class for dealing with the primary functions
@@ -6,9 +6,48 @@
  *
  * Should be written with the idea that it is going to be
  *    and object in the main class kept in mind.
- *
- *
  */
 public class Game {
+
+    // Class variables      ----------
+
+    // TODO
+    // Board array / arraylist / linked list // tree
+    // True atom array / arraylist
+    // Current Score variable
+    // Store past rays and their path here? - Could be stored elsewhere as well
+
+    /**
+     * Constructor for game class, should only be used once in controller class.
+     */
+    public Game() {
+        // TODO
+    }
+
+    // Class functions      ----------
+
+    /**
+     * Calculates the path and result of given ray
+     * @param r Ray object to be used.
+     */
+    public void calculateRay(Ray r) {
+        // TODO
+    }
+
+    /**
+     * Uses Random to create atom placements on board
+     */
+    public void setAtoms() {
+        // TODO
+    }
+
+    /**
+     * Checks given atom flags with true atoms,
+     *    calculates score, and ends the game
+     * @param atomFlags Array of flags to be checked with true atoms
+     */
+    public void submitGame(Atom[] atomFlags) {
+
+    }
 
 }
