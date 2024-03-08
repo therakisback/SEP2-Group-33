e831291f4d083881dbc1a7e01def043c682c0c39
rak <koenigryan4@gmail.com>
2024-03-08 17:25:57 +0000

    Moved test files to proper folder, and created GameTest file

--- Project File - Now with JFX/src/main/java/org/example/project_filenow_with_jfx/Game.java
+++ Project File - Now with JFX/src/main/java/org/example/project_filenow_with_jfx/Game.java
@@ -87,7 +87,6 @@ public class Game {
                 r.setEnd(edge);
                 break;
             }
-
         }
     }
 
---
+++ Project File - Now with JFX/src/test/java/org/example/project_filenow_with_jfx/GameTest.java
@@ -0,0 +1,7 @@
+package org.example.project_filenow_with_jfx;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+}
