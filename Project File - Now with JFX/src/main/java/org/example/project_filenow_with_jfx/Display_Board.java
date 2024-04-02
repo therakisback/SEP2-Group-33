08af30a043827fae04f8d913a071672fedbb7407
Conor Kiely <conor.kiely@ucdconnect.ie>
2024-04-02 19:41:24 +0100

    Added Ray Casting to the Display, (still in progress). End Game Button now appears as discussed. Hexagons and sides (objects) are now numbered and organised in an array.

--- Project File - Now with JFX/src/main/java/org/example/project_filenow_with_jfx/Display_Board.java
+++ Project File - Now with JFX/src/main/java/org/example/project_filenow_with_jfx/Display_Board.java
@@ -5,7 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -14,6 +16,7 @@ public class Display_Board extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game-board.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
         stage.setTitle("Black Box");
         stage.setScene(scene);
 
--- Project File - Now with JFX/src/main/java/org/example/project_filenow_with_jfx/GameController.java
+++ Project File - Now with JFX/src/main/java/org/example/project_filenow_with_jfx/GameController.java
@@ -3,9 +3,12 @@ package org.example.project_filenow_with_jfx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.shape.Polygon;
 import javafx.scene.input.MouseEvent;
@@ -13,22 +16,87 @@ import javafx.scene.input.MouseEvent;
 import java.util.HashMap;
 
 public class GameController {
+    //The Polygons and rectangles represent the hexagons and sides on the board
+    // I've put them into arrays to allow for easier manipulation
     @FXML
     public Polygon zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour,
-    oneZero, oneOne, oneTwo, oneThree, oneFour, oneFive,
-    twoZero, twoOne, twoTwo, twoThree, twoFour, twoFive, twoSix,
-    threeZero, threeOne, threeTwo, threeThree, threeFour, threeFive, threeSix, threeSeven,
-    fourZero, fourOne, fourTwo, fourThree, fourFour, fourFive, fourSix, fourSeven, fourEight,
-    fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour, fiveFive, fiveSix, fiveSeven,
-    sixZero, sixOne, sixTwo, sixThree, sixFour, sixFive, sixSix,
-    sevenZero, sevenOne, sevenTwo, sevenThree, sevenFour, sevenFive,
-    eightZero, eightOne, eightTwo, eightThree, eightFour;
+            oneZero, oneOne, oneTwo, oneThree, oneFour, oneFive,
+            twoZero, twoOne, twoTwo, twoThree, twoFour, twoFive, twoSix,
+            threeZero, threeOne, threeTwo, threeThree, threeFour, threeFive, threeSix, threeSeven,
+            fourZero, fourOne, fourTwo, fourThree, fourFour, fourFive, fourSix, fourSeven, fourEight,
+            fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour, fiveFive, fiveSix, fiveSeven,
+            sixZero, sixOne, sixTwo, sixThree, sixFour, sixFive, sixSix,
+            sevenZero, sevenOne, sevenTwo, sevenThree, sevenFour, sevenFive,
+            eightZero, eightOne, eightTwo, eightThree, eightFour;
 
-    public Circle atom;
+    public Polygon[] array_of_hexagons = {zeroZero, zeroOne, zeroTwo, zeroThree, zeroFour,
+            oneZero, oneOne, oneTwo, oneThree, oneFour, oneFive,
+            twoZero, twoOne, twoTwo, twoThree, twoFour, twoFive, twoSix,
+            threeZero, threeOne, threeTwo, threeThree, threeFour, threeFive, threeSix, threeSeven,
+            fourZero, fourOne, fourTwo, fourThree, fourFour, fourFive, fourSix, fourSeven, fourEight,
+            fiveZero, fiveOne, fiveTwo, fiveThree, fiveFour, fiveFive, fiveSix, fiveSeven,
+            sixZero, sixOne, sixTwo, sixThree, sixFour, sixFive, sixSix,
+            sevenZero, sevenOne, sevenTwo, sevenThree, sevenFour, sevenFive,
+            eightZero, eightOne, eightTwo, eightThree, eightFour};
 
+    @FXML
+    public Rectangle zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen,
+            fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty, twentyOne, twentyTwo, twentyThree,
+            twentyFour, twentyFive, twentySix, twentySeven, twentyEight, twentyNine, thirty, thirtyOne, thirtyTwo,
+            thirtyThree, thirtyFour, thirtyFive, thirtySix, thirtySeven, thirtyEight, thirtyNine, forty, fortyOne,
+            fortyTwo, fortyThree, fortyFour, fortyFive, fortySix, fortySeven, fortyEight, fortyNine, fifty, fiftyOne,
+            fiftyTwo, fiftyThree;
 
-    private int flag = 0;
+    public Rectangle[] array_of_sides = {zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen,
+            fourteen, fifteen, sixteen, seventeen, eighteen, nineteen, twenty, twentyOne, twentyTwo, twentyThree,
+            twentyFour, twentyFive, twentySix, twentySeven, twentyEight, twentyNine, thirty, thirtyOne, thirtyTwo,
+            thirtyThree, thirtyFour, thirtyFive, thirtySix, thirtySeven, thirtyEight, thirtyNine, forty, fortyOne,
+            fortyTwo, fortyThree, fortyFour, fortyFive, fortySix, fortySeven, fortyEight, fortyNine, fifty, fiftyOne,
+            fiftyTwo, fiftyThree};
+
+    public int flag = 0;
+    @FXML
+    private Button endGameButton;
     public Game game = new Game();
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private Button castRayButton;
+    @FXML
+    private void castRay() {
+        // Get user input from the TextField
+        String userInput = inputField.getText();
+
+        // Convert the user input to an integer
+        int methodNumber;
+        try {
+            methodNumber = Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+            return;
+        }
+
+        // Determine where to cast ray from based on the user input
+        if(methodNumber >= 0 && methodNumber <= 53){
+            System.out.println("ray cast from " + methodNumber);
+            switch(methodNumber){
+                case 0:
+                    zero.setOpacity(1);
+                    zero.setFill(Color.RED);
+                    twentySeven.setOpacity(1);
+                    twentySeven.setFill(Color.RED);
+                    break;
+                default:
+                    break;
+            }
+            //TODO
+        }
+        else {
+            System.out.println("Invalid method number. Please enter a valid method number.");
+        }
+
+    }
 
     @FXML
     private void createAtom(MouseEvent e) {
@@ -42,16 +110,12 @@ public class GameController {
         Atom atom = new Atom(atom_place[1], atom_place[0], false);
         System.out.println("Atom created, " + atom.toString());
     }
-    /*@FXML
-    private void endGame(MouseEvent e) {
-        Circle source = (Circle) e.getSource();
-        //source.setOpacity(1);
-        //source.setId();
-        if(atom.getOpacity() == 0){
-            atom.setOpacity(1);
-        }
+    @FXML
+    public void endGame(ActionEvent e) {//when four flags are placed, button appears to end the Game
 
-    }*/
+        System.out.println("Game Over");
+        //switch to new scene
+    }
 
     private int[] convertToNumbers(String[] parts) {
         int[] array = new int[2];
@@ -73,18 +137,29 @@ public class GameController {
     }
 
     @FXML
-    private void placeAtom(MouseEvent e) {
-
+    private void placeAtom(MouseEvent e) {//allows you to place up to 4 flags
         Atom a = new Atom(3,3 ,true);
         Polygon source = (Polygon) e.getSource();
         if(source.getFill() == Color.YELLOW) {
             source.setFill(Color.DODGERBLUE);
             System.out.println("Removed atom flag");
+            flag--;
+            endGameButton.setOpacity(0);
         }
         else{
-            source.setFill(Color.YELLOW);
-            System.out.println("Placed atom flag");
+            if(flag < 3){
+                source.setFill(Color.YELLOW);
+                System.out.println("Placed atom flag");
+                flag++;
+            }
+            else if(flag < 4){
+                source.setFill(Color.YELLOW);
+                System.out.println("Placed atom flag");
+                flag++;
+                endGameButton.setOpacity(1);
+            }
         }
+
     }
 
     @FXML
@@ -107,12 +182,5 @@ public class GameController {
             stage.close();
         }
 
-    }
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
